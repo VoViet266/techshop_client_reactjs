00b59b95e272d5a04f0f89e87d1705127481e5ce
lai-tran-the-vinh <thevinh.development@gmail.com>
2025-06-25 15:03:16 +0700

    feat: Enhance product filtering and display in ListProducts page with dynamic options for price, color, RAM, and storage

--- client/src/pages/users/ListProducts.jsx
+++ client/src/pages/users/ListProducts.jsx
@@ -4,16 +4,55 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Categories from '@services/categories';
 import { callFetchProducts } from '@services/apis';
-import { Typography, Row, Col, Tag, Space, Skeleton, Select } from 'antd';
+import { Typography, Flex, Row, Col, Tag, Space, Skeleton, Select } from 'antd';
 
 function ProductListPage() {
   const { id } = useParams();
   const { message } = useAppContext();
+  const [colors, setColors] = useState([]);
+  const [rams, setRams] = useState([]);
+  const [storages, setStorages] = useState([]);
   const [brands, setBrands] = useState([]);
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [category, setCategory] = useState(null);
   const [currentBrand, setCurrentBrand] = useState('');
+  const [filter, setFilter] = useState({
+    price: null,
+    color: null,
+    ram: null,
+    storage: null,
+  });
+
+  const filteredProducts = products.filter((product) => {
+    // Lọc theo giá
+    let matchPrice = true;
+    const realPrice =
+      product?.variants?.[0]?.price -
+      product?.variants?.[0]?.price * (product?.discount / 100);
+    if (filter.price === 1) {
+      matchPrice = realPrice < 10000000;
+    } else if (filter.price === 2) {
+      matchPrice = realPrice >= 10000000 && realPrice <= 20000000;
+    } else if (filter.price === 3) {
+      matchPrice = realPrice > 20000000;
+    }
+
+    // Lọc theo màu
+    let matchColor = filter.color
+      ? product.variants?.some((v) => v.color.name === filter.color)
+      : true;
+    // Lọc theo RAM
+    let matchRam = filter.ram
+      ? product.variants?.some((v) => v.memory.ram === filter.ram)
+      : true;
+    // Lọc theo bộ nhớ trong
+    let matchStorage = filter.storage
+      ? product.variants?.some((v) => v.memory.storage === filter.storage)
+      : true;
+
+    return matchPrice && matchColor && matchRam && matchStorage;
+  });
 
   useEffect(() => {
     if (products.length > 0 && brands.length === 0) {
@@ -22,6 +61,33 @@ function ProductListPage() {
         ...new Set(products.map((product) => product.brand.name)),
       ];
       setBrands(brands);
+
+      const colors = [
+        ...new Set(
+          products
+            .flatMap((p) => p.variants?.map((v) => v.color))
+            .filter(Boolean),
+        ),
+      ];
+      setColors(colors);
+
+      const rams = [
+        ...new Set(
+          products
+            .flatMap((p) => p.variants?.map((v) => v.memory.ram))
+            .filter(Boolean),
+        ),
+      ];
+      setRams(rams);
+
+      const storages = [
+        ...new Set(
+          products
+            .flatMap((p) => p.variants?.map((v) => v.memory.storage))
+            .filter(Boolean),
+        ),
+      ];
+      setStorages(storages);
     }
   }, [products]);
 
@@ -68,12 +134,91 @@ function ProductListPage() {
             {category.name}
           </Typography.Title>
         )}
+      </div>
+
+      <div>
+        {brands.map((brand, index) => (
+          <Tag
+            key={index}
+            onClick={(event) => {
+              const tag = event.target.textContent;
+              if (tag === 'Tất cả') {
+                setCurrentBrand('');
+                return;
+              }
+              setCurrentBrand(brand);
+            }}
+            className={`font-roboto! text-sm! px-8! border-none! rounded-md! cursor-pointer! ${currentBrand === brand && 'bg-gray-200!'}  min-w-80! text-center! bg-gray-100! py-4! mb-12!`}
+          >
+            {brand}
+          </Tag>
+        ))}
+      </div>
+
+      <Flex className="mb-12!" justify="space-between">
         <Space>
-          <Typography.Text className="font-medium!">
-            Lọc theo giá
+          <Space>
+            <Typography.Text className="font-medium!">Giá</Typography.Text>
+            <Select
+              placeholder="Khoảng giá"
+              className="min-w-120!"
+              allowClear
+              options={[
+                { value: 1, label: 'Dưới 10 triệu' },
+                { value: 2, label: '10 - 20 triệu' },
+                { value: 3, label: 'Trên 20 triệu' },
+              ]}
+              onChange={(value) => {
+                setFilter((f) => ({ ...f, price: value }));
+              }}
+            />
+          </Space>
+          <Space>
+            <Typography.Text className="font-medium!">Màu sắc</Typography.Text>
+            <Select
+              placeholder="Màu sắc"
+              className="min-w-120!"
+              allowClear
+              options={colors.map((color) => ({
+                value: color.name,
+                label: color.name,
+              }))}
+              onChange={(value) => setFilter((f) => ({ ...f, color: value }))}
+            />
+          </Space>
+          <Space>
+            <Typography.Text className="font-medium!">RAM</Typography.Text>
+            <Select
+              placeholder="RAM"
+              className="min-w-120!"
+              allowClear
+              options={rams.map((ram) => ({ value: ram, label: ram }))}
+              onChange={(value) => setFilter((f) => ({ ...f, ram: value }))}
+            />
+          </Space>
+          <Space>
+            <Typography.Text className="font-medium!">
+              Bộ nhớ trong
+            </Typography.Text>
+            <Select
+              placeholder="Bộ nhớ trong"
+              className="min-w-120!"
+              allowClear
+              options={storages.map((storage) => ({
+                value: storage,
+                label: storage,
+              }))}
+              onChange={(value) => setFilter((f) => ({ ...f, storage: value }))}
+            />
+          </Space>
+        </Space>
+        <Space>
+          <Typography.Text className="font-medium! ml-auto!">
+            Sắp xếp theo giá
           </Typography.Text>
           <Select
             showSearch
+            allowClear
             placeholder="Sắp xếp theo giá"
             className="cursor-pointer! min-w-200!"
             options={[
@@ -96,25 +241,7 @@ function ProductListPage() {
             }}
           />
         </Space>
-      </div>
-      <div>
-        {brands.map((brand, index) => (
-          <Tag
-            key={index}
-            onClick={(event) => {
-              const tag = event.target.textContent;
-              if (tag === 'Tất cả') {
-                setCurrentBrand('');
-                return;
-              }
-              setCurrentBrand(brand);
-            }}
-            className={`font-roboto! text-sm! px-8! border-none! rounded-md! cursor-pointer! ${currentBrand === brand && 'bg-gray-200!'}  min-w-80! text-center! bg-gray-100! py-4! mb-12!`}
-          >
-            {brand}
-          </Tag>
-        ))}
-      </div>
+      </Flex>
       <Row gutter={10} justify="start">
         {loading && (
           <>
@@ -136,7 +263,7 @@ function ProductListPage() {
           </>
         )}
         {!loading &&
-          products.map((product, index) => {
+          filteredProducts.map((product, index) => {
             return (
               <Col span={5} key={index}>
                 <Card
