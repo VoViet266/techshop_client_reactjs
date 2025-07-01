0f6addcfabef748d9e450e00c98804fe2ea137f2
lai-tran-the-vinh <thevinh.development@gmail.com>
2025-07-01 13:11:08 +0700

    feat: Update cart functionality to fetch items from API and improve item handling

--- client/src/services/carts/index.js
+++ client/src/services/carts/index.js
@@ -4,6 +4,10 @@ class CartServices {
   add(items) {
     return axiosInstance.post('/api/v1/carts', { items });
   }
+  
+  get() {
+    return axiosInstance.get('/api/v1/carts');
+  }
 }
 
 export default CartServices;
